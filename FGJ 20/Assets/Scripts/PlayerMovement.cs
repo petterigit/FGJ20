e21ea606833438e46db50267425dae55e7c9bd94
petterigit <33552590+petterigit@users.noreply.github.com>
2020-02-01 10:40:51 +0200

    aaaaaa

--- FGJ 20/Assets/Scripts/PlayerMovement.cs
+++ FGJ 20/Assets/Scripts/PlayerMovement.cs
@@ -13,41 +13,63 @@ public class PlayerMovement : MonoBehaviour
 
     private bool hammertime = false;
 
-    void Start()
-    {
-    }
+    
+
+    
 
     // Update is called once per frame
     void FixedUpdate()
     {
 		
-        float v = Input.GetAxis(vertical);
-		float h = Input.GetAxis(horizontal);
-		
-		float dy = v * speed * Time.deltaTime;
-		float dx = h * speed * Time.deltaTime;
+        
+        Move(speed);
 
-        if ( (dy != 0) || (dx != 0) ) {
-            animator.SetFloat("Speed", 1);
-        } else {
-            animator.SetFloat("Speed", 0);
-        }
+        hammertime = Repair(hammertime);
+        
+        
+    }
 
-		Vector2 newPosition = new Vector2(transform.position.x+dx, transform.position.y+dy);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, newPosition);
-        if (hit.collider != null) {
-            //Debug.Log("Ping");
-        }
-		transform.position = newPosition;
+    
 
+    void Move(float speed) 
+    {
+            float v = Input.GetAxis(vertical);
+            float h = Input.GetAxis(horizontal);
+            
+            float dy = v * speed * Time.deltaTime;
+            float dx = h * speed * Time.deltaTime;
+
+            if ( (dy != 0) || (dx != 0) ) {
+                animator.SetFloat("Speed", 1);
+            } else {
+                animator.SetFloat("Speed", 0);
+            }
+
+            Vector2 newPosition = new Vector2(transform.position.x+dx, transform.position.y+dy);
+
+            transform.position = newPosition;
+    }
+
+    bool Repair(bool hammertime) {
         if (Input.GetButton(firebutton)) {
-            //Debug.Log("Ping " + firebutton);
             hammertime = true;
             animator.SetBool("Hammertime", hammertime);
         } else {
             hammertime = false;
             animator.SetBool("Hammertime", hammertime);
         }
-        
+        return hammertime;
     }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.tag + " : " + gameObject.name + " : " + Time.time);
+        if (col.gameObject.tag == "boat") {
+            speed = 3;
+        } else if (col.gameObject.tag == "water" ) {
+            speed = 1;
+        }
+
+    }
+    
 }
