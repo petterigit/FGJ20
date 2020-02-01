4c60b612a42cafb2c09d3214e6fd049db58f9ef3
nikugronberg <niku.gronberg@student.lut.fi>
2020-02-01 16:34:30 +0200

    fixered

---
+++ FGJ 20/Assets/Scripts/InputComboGeneration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputComboGeneration : MonoBehaviour
+{
+    public string[] inputs;
+    
+    /*
+    public string[] CreateCombo(string playerid, ) {
+        string[] combo = 
+    }
+    */
+}
