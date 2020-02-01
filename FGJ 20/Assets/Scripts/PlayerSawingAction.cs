13266ebc8d43d47bedf2f8919ab7bc32ceb2440e
nikugronberg <niku.gronberg@student.lut.fi>
2020-02-01 13:47:58 +0200

    destruction

---
+++ FGJ 20/Assets/Scripts/PlayerSawingAction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSawingAction : MonoBehaviour
+{
+    public string actionButton;
+    public BoatController bc;
+
+    [SerializeField]
+    private bool isSawing = false;
+
+    [SerializeField]
+    private Vector2 sawStart;
+    [SerializeField]
+    private Vector2 sawEnd;
+
+    private Camera cam;
+
+    private Vector2 initVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+
+        initVector = new Vector2(0, 0);
+        sawStart = initVector;
+        sawEnd = initVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown(actionButton))
+        {
+            isSawing = true;
+            sawStart = transform.position;
+
+        }
+
+        if (Input.GetButtonUp(actionButton) && isSawing)
+        {
+            isSawing = false;
+            sawEnd = transform.position;
+        }
+
+        if(sawStart != initVector && sawEnd != initVector)
+        {
+            bc.Saw(sawStart, sawEnd);
+            sawStart = initVector;
+            sawEnd = initVector;
+        }
+
+    }    
+}
